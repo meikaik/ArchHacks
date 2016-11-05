554d4c34efc139c1c31524b0919ac932c458cad3
meikaik <meikaikoh@gmail.com>
2016-11-05 00:50:14 -0500

    Initial Commit

---
+++ ArchHacks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArchHacks
+//
+//  Created by Mei Kai Koh on 5/11/16.
+//  Copyright © 2016 UW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
